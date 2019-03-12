536155228a1ab028573efd09e53147092614d1e7
tlei200 <tao_lei@cable.comcast.com>
2019-03-12 17:47:15 -0400

    add event emitter

--- example/fetch-danmaku.mjs
+++ example/fetch-danmaku.mjs
@@ -3,10 +3,36 @@ import { DanmakuClient } from '../src/lib/danmaku/danmaku.mjs'
 const danmakuClient = new DanmakuClient({
     url: 'openbarrage.douyutv.com',
     port: '8601',
+    roomId: '3733001',
 });
 
 try {
     danmakuClient.start();
+    let previousRemainder = Buffer.alloc(1);
+
+    danmakuClient.on('connected', () => {
+        console.log('ready');
+        danmakuClient.connectToRoom();
+        danmakuClient.joinGroup("-9999");
+        danmakuClient.keepAlive(40000);
+    });
+
+    danmakuClient.on('data', data => {
+        console.log('data received');
+
+        const handledMessage = danmakuClient.handleMessage(data, previousRemainder);
+        previousRemainder = handledMessage.remainder;
+
+        console.log(handledMessage);
+        // console.log(danmakuClient.decode(data));
+    
+    });
+
+    danmakuClient.on('closed', () => {
+        console.log('connection closed');
+        console.log('reconnect');
+        danmakuClient.start();
+    })
 } catch (ex) {
     console.log(ex);
 }
--- src/lib/danmaku/danmaku.mjs
+++ src/lib/danmaku/danmaku.mjs
@@ -1,71 +1,386 @@
 import net from 'net';
+import EventEmitter from 'events';
 
 const HEADER_LENGTH = 8;
 const FRAME_LENGTH = 4;
+const HEADER_SIZE = 4;
+const HEADER_TYPECODE = 2;
+const HEADER_ENCRYPT = 1;
+const HEADER_RESERVED = 1;
+
 const regex = 'txt@=(.+?)/cid@';
-class DanmakuClient {
+class DanmakuClient extends EventEmitter{
     constructor(options) {
+        super();
         this.url = options.url;
         this.port = options.port;
-        this.roomId = options.roomId;
-        this.roomId = '1126960';
+        this.roomId = options.roomId;;
         this.socket = new net.Socket();
-
-    }
-
-    async start() {
-        await this.socket.connect(this.port, this.url);
-
-
-        this.socket.on('connect', () => {
-            console.log('connected');
-            this.connectToRoom();
-            console.log('connected to room');
-            this.joinGroup();
-            console.log('joined group -9999');
+        this.socket.on('ready', () => {
+            this.emit('connected');
         });
-
         this.socket.on('data', (data) => {
-            console.log('received data')
-            const danmaku = data.toString('utf8', HEADER_LENGTH + FRAME_LENGTH).match(regex);
-            if (danmaku) {
-                console.log('======danmaku======')
-                console.log(danmaku[1]);
-            }
-            
+            this.emit('data', data);
         });
-        this.socket.on('error', (err) => {
-            console.log(err)
+
+        this.socket.on('close', () => {
+            this.emit('closed');
         })
     }
-
-    joinGroup() {
-        const message = `type@=joingroup/rid@=${this.roomId}/gid@=-9999/\0`;
-        const buffer = this.encodeMessage(message);
-        this.socket.write(buffer);
+    /**
+     * [start: connect to Douyu danmaku server]
+     */
+    start() {
+        this.socket.connect(this.port, this.url);
     }
 
-    connectToRoom() {
-        const message = `type@=loginreq/username@=taolei/password@=douyu/roomid@=${this.roomId}/\0`;
-        const buffer = this.encodeMessage(message);
-        this.socket.write(buffer);
-    }
-
-    encodeMessage(message) {
-        const messageLen = message.length;
+    /**
+     * [encode: encode the message sent to Douyu danmaku server]
+     * Message format, please refer to 斗鱼弹幕服务器第三方接入协议v1.6.2.pdf for more details
+     * Message length, 4 byte little endian integer
+     * Message length, 4 byte little endian integer(repeat)
+     * Message type, 2 byte little endian integer
+     *  689: client sending message to server
+     *  690 server sending message to client
+     * Encrypt, 1 byte little endian integer, unused, default to 0
+     * Reserved, 1 byte little endian integer, unused, default to 0
+     * Body, must end with '\0'
+     * @param   {string}    message     [message to be encoded]
+     * @return  {Buffer}                [encoded buffer]                    
+     */
+    encode(message) {
         const code = 689;
-        const bufferHead = new Buffer(HEADER_LENGTH + FRAME_LENGTH);
+        const bufferHeader = new Buffer(HEADER_LENGTH + FRAME_LENGTH);
         const bufferMessage = new Buffer(message, 'utf8');
         const totalBufferLength = message.length + HEADER_LENGTH + FRAME_LENGTH;
-        bufferHead.writeInt32LE(totalBufferLength - FRAME_LENGTH, 0);
-        bufferHead.writeInt32LE(totalBufferLength - FRAME_LENGTH, 4);
-        bufferHead.writeInt16LE(code, 8);
-        bufferHead.writeInt16LE(0,10);
-        return  Buffer.concat([bufferHead, bufferMessage], totalBufferLength);
+        bufferHeader.writeInt32LE(totalBufferLength - FRAME_LENGTH, 0);
+        bufferHeader.writeInt32LE(totalBufferLength - FRAME_LENGTH, 4);
+        bufferHeader.writeInt16LE(code, 8);
+        bufferHeader.writeInt16LE(0,10);
+        return Buffer.concat([bufferHeader, bufferMessage], totalBufferLength);
+        // this.socket.write(Buffer.concat([bufferHead, bufferMessage], totalBufferLength));        
     }
 
+    /**
+     * [decode: decode the message received from Douyu danmaku server]
+     * @param   {Buffer}    buffer      [buffer to be decoded]
+     * @return  {string}                [decoded string from buffer]
+     */
+    decode(buffer) {
+        return buffer.toString('utf8', HEADER_LENGTH + FRAME_LENGTH);
+    }
+    /**
+     * [joinGroup: send a message to Douyu danmaku server to join a danmaku group]
+     * @param   {string}    gourpId      [group id of the danmaku group, select -9999 to join unlimited danmaku group] 
+     */
+    joinGroup(gourpId) {
+        const message = `type@=joingroup/rid@=${this.roomId}/gid@=${gourpId}/\0`;
+        this.socket.write(this.encode(message));
+    }
 
+    /**
+     * [connectToRoom: send a message to Douyu danmaku server to join a room]
+     */
+    connectToRoom() {
+        const message = `type@=loginreq/username@=tao.lei/password@=douyu/roomid@=${this.roomId}/\0`;
+        this.socket.write(this.encode(message));
+    }
+
+    /**
+     * [keepAlive: periodically send a heartbeat message to Douyu danmaku server to maintain the socket connection]
+     * Douyu danmaku server requires a heartbeat signal to be sent every 45s
+     * @param   {integer}   interval        period of the  heartbeat signal in ms
+     */
+    keepAlive(interval) {
+        // const message = `type@=keeplive/tick@${Math.floor(Date.now()/1000)}`;    //old heartbeat message
+        const message = `type@=mrkl/`                                               //new heartbeat message
+        setInterval( () => {
+            this.socket.write(this.encode(message));
+        }, interval);
+    }
+
+    /**
+     * [handleMessage: decode and deserialize the message received from Douyu danmaku server]
+     * @param   {buffer}    rawMessage          message received from Douyu danmaku server
+     * @param   {string}    previousRemainder   the previously received message's unhandled part
+     * @return  {Object}                        Array of JSON of the decoded and deserialized message and remaining message string to be processed next time
+     */
+    handleMessage(rawMessage, previousRemainder) {
+        const deserializeRecords = [];
+        if (rawMessage === null) {
+            return {
+                deserializeRecords,
+                remainder: previousRemainder
+            }
+        }
+
+        // Decode the buffer message to string
+        const decodedMessage = this.decode(rawMessage);
+
+        const fullMessage = previousRemainder + decodedMessage;
+        const splitPosition = fullMessage.lastIndexOf('type@=');
+
+
+        if (fullMessage.indexOf('type@=') === splitPosition) {
+            return {
+                deserializeRecords,
+                remainder: fullMessage
+            }
+        }
+        const processingMessage = fullMessage.substring(0, splitPosition);
+
+        const remainder = fullMessage.substring(splitPosition);
+        console.log('============full================')
+        console.log(processingMessage);
+        console.log('============rema================')
+        console.log(remainder)
+        console.log('============end================')
+        const rawRecords = processingMessage.split('type@=');
+
+        rawRecords.forEach(rawRecord => {
+            const deserializeRecord = this.deserialize(rawRecord);
+            if (deserializeRecord) {
+                deserializeRecords.push(this.deserialize(rawRecord)) ;
+            }
+        })
+        return {
+            deserializeRecords,
+            remainder,
+        }
+    }
+
+    deserialize(rawRecord) {
+        if (rawRecord.length <= 0) {
+            //TODO: throw error
+            return null;
+        }
+
+        const data = rawRecord.split('/');
+
+        if(data.length <= 1) {
+            //TODO: throw error
+            return null;
+        }
+
+        if(data[0].lastIndexOf('@=') != -1) {
+            //TODO: throw error
+            return null;            
+        }
+
+        const recordType = data.shift();
+        const deserializeRecord = {
+            recordType,
+        }
+
+        data.forEach( keyValuePair => {
+            const keyValueArray = keyValuePair.split('@=');
+            const key = keyValueArray[0];
+            const value = keyValueArray[1];
+            deserializeRecord[key] = value
+        });
+
+        return deserializeRecord;
+    }
 }
 
 export { DanmakuClient };
 export default DanmakuClient;
+
+
+
+
+
+
+
+
+
+// import net from 'net';
+// import EventEmitter from 'events';
+
+// const HEADER_LENGTH = 8;
+// const FRAME_LENGTH = 4;
+// const HEADER_SIZE = 4;
+// const HEADER_TYPECODE = 2;
+// const HEADER_ENCRYPT = 1;
+// const HEADER_RESERVED = 1;
+
+// const regex = 'txt@=(.+?)/cid@';
+// class DanmakuClient extends EventEmitter{
+//     constructor(options) {
+//         super();
+//         this.url = options.url;
+//         this.port = options.port;
+//         this.roomId = options.roomId;;
+//         this.socket = new net.Socket();
+//         this.socket.on('ready', () => {
+//             this.emit('connected');
+//         });
+//         this.socket.on('data', (data) => {
+//             this.emit('data', data);
+//         });
+
+//         this.socket.on('close', () => {
+//             this.emit('closed');
+//         })
+//     }
+//     /**
+//      * [start: connect to Douyu danmaku server]
+//      */
+//     start() {
+//         this.socket.connect(this.port, this.url);
+//     }
+
+//     /**
+//      * [encode: encode the message sent to Douyu danmaku server]
+//      * Message format, please refer to 斗鱼弹幕服务器第三方接入协议v1.6.2.pdf for more details
+//      * Message length, 4 byte little endian integer
+//      * Message length, 4 byte little endian integer(repeat)
+//      * Message type, 2 byte little endian integer
+//      *  689: client sending message to server
+//      *  690 server sending message to client
+//      * Encrypt, 1 byte little endian integer, unused, default to 0
+//      * Reserved, 1 byte little endian integer, unused, default to 0
+//      * Body, must end with '\0'
+//      * @param   {string}    message     [message to be encoded]
+//      * @return  {Buffer}                [encoded buffer]                    
+//      */
+//     encode(message) {
+//         const code = 689;
+//         const bufferHeader = new Buffer(HEADER_LENGTH + FRAME_LENGTH);
+//         const bufferMessage = new Buffer(message, 'utf8');
+//         const totalBufferLength = message.length + HEADER_LENGTH + FRAME_LENGTH;
+//         bufferHeader.writeInt32LE(totalBufferLength - FRAME_LENGTH, 0);
+//         bufferHeader.writeInt32LE(totalBufferLength - FRAME_LENGTH, 4);
+//         bufferHeader.writeInt16LE(code, 8);
+//         bufferHeader.writeInt16LE(0,10);
+//         return Buffer.concat([bufferHeader, bufferMessage], totalBufferLength);
+//         // this.socket.write(Buffer.concat([bufferHead, bufferMessage], totalBufferLength));        
+//     }
+
+//     /**
+//      * [decode: decode the message received from Douyu danmaku server]
+//      * @param   {Buffer}    buffer      [buffer to be decoded]
+//      * @return  {string}                [decoded string from buffer]
+//      */
+//     decode(buffer) {
+//         let pos = 0;
+//         let messageSize = 0;
+//         const totalHeaderSize = HEADER_SIZE * 2 + HEADER_TYPECODE + HEADER_ENCRYPT + HEADER_RESERVED;
+//         const decodedMessages = []
+
+//         do {
+//             messageSize = buffer.readInt32LE(pos);
+//             console.log('====msg size=====')
+//             console.log(messageSize)
+//             console.log(buffer.readInt32LE(pos + HEADER_SIZE))
+
+//             const decodedMessage = buffer.toString('utf8', pos + totalHeaderSize + 1, pos + totalHeaderSize + messageSize);
+//             pos += (totalHeaderSize + messageSize);
+//             decodedMessages.push(decodedMessage);
+            
+//         } while (pos < buffer.byteLength);
+
+//         let remainingBuffer = Buffer.alloc(1);
+
+//         if (pos !== buffer.byteLength - 1) {
+//             remainingBuffer = buffer.slice(pos - totalHeaderSize - messageSize)
+//         }
+//         console.log('====decode=====')
+//         console.log({
+//             decodedMessages,
+//             remainingBuffer,
+//         })
+//         return {
+//             decodedMessages,
+//             remainingBuffer,
+//         }
+//     }
+//     /**
+//      * [joinGroup: send a message to Douyu danmaku server to join a danmaku group]
+//      * @param   {string}    gourpId      [group id of the danmaku group, select -9999 to join unlimited danmaku group] 
+//      */
+//     joinGroup(gourpId) {
+//         const message = `type@=joingroup/rid@=${this.roomId}/gid@=${gourpId}/\0`;
+//         this.socket.write(this.encode(message));
+//     }
+
+//     /**
+//      * [connectToRoom: send a message to Douyu danmaku server to join a room]
+//      */
+//     connectToRoom() {
+//         const message = `type@=loginreq/username@=tao.lei/password@=douyu/roomid@=${this.roomId}/\0`;
+//         this.socket.write(this.encode(message));
+//     }
+
+//     /**
+//      * [keepAlive: periodically send a heartbeat message to Douyu danmaku server to maintain the socket connection]
+//      * Douyu danmaku server requires a heartbeat signal to be sent every 45s
+//      * @param   {integer}   interval        period of the  heartbeat signal in ms
+//      */
+//     keepAlive(interval) {
+//         // const message = `type@=keeplive/tick@${Math.floor(Date.now()/1000)}`;    //old heartbeat message
+//         const message = `type@=mrkl/`                                               //new heartbeat message
+//         setInterval( () => {
+//             this.socket.write(this.encode(message));
+//         }, interval);
+//     }
+
+//     /**
+//      * [handleMessage: decode and deserialize the message received from Douyu danmaku server]
+//      * @param   {buffer}    bufferMessage          message received from Douyu danmaku server
+//      * @param   {string}    remainingBuffer   the previously received message's unhandled part
+//      * @return  {Object}                        Array of JSON of the decoded and deserialized message and remaining message string to be processed next time
+//      */
+//     handleMessage(bufferMessage, previousRemainingBuffer) {
+//         let deserializeRecords = [];
+//         if (bufferMessage === null) {
+//             return {
+//                 deserializeRecords,
+//                 remainder: previousRemainingBuffer
+//             }
+//         }
+
+//         // Decode the buffer message to string
+//         let fullBufferMessage;
+//         if (previousRemainingBuffer.length > 0) {
+//             fullBufferMessage = Buffer.concat([previousRemainingBuffer, bufferMessage]);
+//         } else {
+//             fullBufferMessage = bufferMessage
+//         }
+//         const { decodedMessages, remainingBuffer } = this.decode(fullBufferMessage);
+
+//         deserializeRecords = decodedMessages.map( decodedMessage => this.deserialize(decodedMessage));
+//         console.log({remainingBuffer})
+//         return {
+//             deserializeRecords,
+//             remainder: remainingBuffer,
+//         }
+//     }
+
+//     deserialize(rawRecord) {
+//         if (rawRecord.length <= 0) {
+//             //TODO: throw error
+//             return null;
+//         }
+
+//         const data = rawRecord.split('/');
+
+//         if(data.length <= 1) {
+//             //TODO: throw error
+//             return null;
+//         }
+//         const deserializeRecord = {};
+
+//         data.forEach( keyValuePair => {
+//             const keyValueArray = keyValuePair.split('@=');
+//             const key = keyValueArray[0];
+//             const value = keyValueArray[1];
+//             deserializeRecord[key] = value
+//         });
+
+//         return deserializeRecord;
+//     }
+// }
+
+// export { DanmakuClient };
+// export default DanmakuClient;
